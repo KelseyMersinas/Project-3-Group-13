2b25fd608cdf4b8855fc5c19d42bc00b1ffd81cb
Kade Rivers <kade.rivers.21@gmail.com>
2024-08-06 19:19:03 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import sqlite3
+
+
 
 app = Flask(__name__)
 
@@ -9,7 +12,14 @@ def main_handler(chart_name):
     }
     return  render_template("index.html", chart_js=js_targets.get(chart_name, "noop.js"))
 
+@app.route("/api/v1/mass_distribution")
+def mass_distribution_api():
+    conn = sqlite3.connect('meteorite-landings.sqlite')
+    cur = conn.cursor()
+    cur.execute("SELECT id, mass, lat, long FROM [meteorite-landings]")
+    rows = cur.fetchall()
 
+    return rows
 
 if __name__ == "__main__":
     app.run(debug=True)
