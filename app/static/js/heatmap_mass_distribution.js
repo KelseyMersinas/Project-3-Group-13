5709228d38647db83727eb92b6ff29d62cb0f4e1
Kade Rivers <kade.rivers.21@gmail.com>
2024-08-08 20:43:51 -0500

    Update to heatmap

--- app/static/js/heatmap_mass_distribution.js
+++ app/static/js/heatmap_mass_distribution.js
@@ -1,44 +1,42 @@
 // Store our API endpoint as queryUrl.
-let queryUrl = "/api/v1/mass_distribution";
+let queryUrl = "api/v1/mass_distribution";
 
 // Perform a GET request to the query URL/
-d3.json(queryUrl).then(function (data) {
-  // Once we get a response, send the data.features object to the createFeatures function.
-  createFeatures(data.features);
+d3.json(queryUrl)
+  .then(data => {
+    console.log(data);
+    createMap(data);
+})
+.catch(error => {
+  console.error('There was a problem with your fetch operation', error);
 });
 
-function createFeatures(meteoriteData) {
+function createMap(data) {
+  let myMap = L.map('map').setView([51.505,-0.09], 13);
+  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  })
 
-  // Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that includes the mass for each meteor
-  function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
-  }
-
-  function generateMarker(feature, latlng){
-    let markerOptions = {
-        radius: feature.properties.mag *5,
-        fillColor: "#BE0303",
-        color: "#410202",
-        weight: 1,
-        opacity: 1,
-        fillOpacity: feature.geometry.coordinates[2] / 40.0
-    };
-    return L.circleMarker(latlng, markerOptions);
-  }
-
-  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Run the onEachFeature function once for each piece of data in the array.
-  let earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature,
-    pointToLayer: generateMarker
+  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
   });
 
-  // Send our earthquakes layer to the createMap function/
-  createMap(earthquakes);
+  let baseMaps = {
+    "Street Map": street,
+    "Topographic Map": topo
+  };
+
+  L.control.layers(baseMaps, {}, {
+    collapsed: false
+  }).addTo(myMap);
 }
 
-function createMap(earthquakes) {
+/*/
+
+function createMap(data) {
+
+// Create map
+let myMap = L.map('map').setView([51.505, -0.09], 13);
 
   // Create the base layers.
   let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -55,19 +53,51 @@ function createMap(earthquakes) {
     "Topographic Map": topo
   };
 
+// {
+  // Once we get a response, send the data.features object to the createFeatures function.
+//   createFeatures(data.features);
+// });
+
+// function createFeatures(meteoriteData) {
+
+  // Define a function that we want to run once for each feature in the features array.
+  // Give each feature a popup that includes the mass for each meteor
+  // function onEachFeature(feature, layer) {
+  //   layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+  // }
+
+  // function generateMarker(feature, latlng){
+  //   let markerOptions = {
+  //       radius: feature.properties.mag *5,
+  //       fillColor: "#BE0303",
+  //       color: "#410202",
+  //       weight: 1,
+  //       opacity: 1,
+  //       fillOpacity: feature.geometry.coordinates[2] / 40.0
+  //   };
+  //   return L.circleMarker(latlng, markerOptions);
+  // }
+
+  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+  // Run the onEachFeature function once for each piece of data in the array.
+  // let meteorites = L.geoJSON(earthquakeData, {
+  //   onEachFeature: onEachFeature,
+  //   pointToLayer: generateMarker
+  // });
+
+  // Send our earthquakes layer to the createMap function/
+ // createMap(earthquakes);
+// }
+
+// function createMap(earthquakes) {
+
+
   // Create an overlay object to hold our overlay.
   let overlayMaps = {
-    Earthquakes: earthquakes
+    Meteorites: meteorites
   };
 
-  // Create our map, giving it the streetmap and earthquakes layers to display on load.
-  let myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 5,
-    layers: [street, earthquakes]
-  });
+
 
   // Create a layer control.
   // Pass it our baseMaps and overlayMaps.
@@ -76,14 +106,15 @@ function createMap(earthquakes) {
     collapsed: false
   }).addTo(myMap);
 
-  // Create a legend
- let legend = L.control({position: "bottomright" });
+//   // Create a legend
+//  let legend = L.control({position: "bottomright" });
 
- legend.onAdd = function (map) {
-    let div = L.DomUtil.create("div", "info legend");
-    div.innerHTML = "<b>Legend</b><br/>Larger circle equals higher magnitude <br/> Darker color equals lower depth"
-    return div;
-}
-legend.addTo(myMap);
+//  legend.onAdd = function (map) {
+//     let div = L.DomUtil.create("div", "info legend");
+//     div.innerHTML = "<b>Legend</b><br/>Larger circle equals higher magnitude <br/> Darker color equals lower depth"
+//     return div;
+// }
+// legend.addTo(myMap);
 
 }
+//*/
